2b7a2a58a7e07e8ffa6cc4ee967b66b51721704e
Mark Riley <markjriley1899@gmail.com>
2018-04-20 12:40:32 +0100

    First Commit and Setup Done

---
+++ GradedUnit/GradedUnit/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace GradedUnit.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        /// <summary>
+        /// Returns the Area Name
+        /// </summary>
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+        /// <summary>
+        /// Defines the Route and Default Action
+        /// </summary>
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
